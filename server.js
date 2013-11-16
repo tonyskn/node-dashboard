463cf0501024edee113b174ccf74c6eb94c0815f
tonyskn <tonyskn@gmail.com>
2013-11-16 13:33:09 +0100

    Basic express server up and running

---
+++ server.js
@@ -0,0 +1,20 @@
+
+var express = require('express'),
+    http = require('http'),
+
+    app = express(),
+    server = http.createServer(app),
+    io = require('socket.io').listen(server);
+
+app.use(express.static('public'));
+
+server.listen(4320);
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
+
+
